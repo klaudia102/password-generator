8af3fc9d9d68f045074859ed5fd04c17a5a21cb3
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-01-13 18:46:22 +0000

    added code for getting random element from array

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -88,45 +88,49 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-  let userResponse = prompt('Please define number of characters in your password - it must be between 8 and 128');
-  userResponse = parseInt(userResponse);
-  console.log(userResponse)
-  if(!userResponse) {
-    console.log('It was not a number')
-    alert('It was not a number')
-  }
-    else if(userResponse >= 8 && userResponse <= 128) {
-      console.log('Success! Thank you!')
-      alert('Success! Thank you!')
-  }
-  else {
-    console.log('Number not withing range, please choose number between 8 and 128')
-    alert('Number not withing range, please choose number between 8 and 128')
-  }
+// // Function to prompt user for password options
+// function getPasswordOptions() {
+//   let userResponse = prompt('Please define number of characters in your password - it must be between 8 and 128');
+//   userResponse = parseInt(userResponse);
+//   console.log(userResponse)
+//   if(!userResponse) {
+//     console.log('It was not a number')
+//     alert('It was not a number')
+//   }
+//     else if(userResponse >= 8 && userResponse <= 128) {
+//       console.log('Success! Thank you!')
+//       alert('Success! Thank you!')
+//   }
+//   else {
+//     console.log('Number not withing range, please choose number between 8 and 128')
+//     alert('Number not withing range, please choose number between 8 and 128')
+//   }
 
-  const getSpecialCharacters = confirm ('Click OK if you want to use special charakters')
-  const getNumericCharacters = confirm ('Click OK if you want to use numbers')
-  const getLowercaseCharacters = confirm ('Click OK if you want to use lowercase characters')
-  const getUppercaseCharacters = confirm ('Click OK if you want to use uppercase characters')
-  console.log(getLowercaseCharacters);
-  console.log(getNumericCharacters);
-  console.log(getUppercaseCharacters);
-  console.log(getSpecialCharacters);
+//   const getSpecialCharacters = confirm ('Click OK if you want to use special charakters')
+//   const getNumericCharacters = confirm ('Click OK if you want to use numbers')
+//   const getLowercaseCharacters = confirm ('Click OK if you want to use lowercase characters')
+//   const getUppercaseCharacters = confirm ('Click OK if you want to use uppercase characters')
+//   console.log(getLowercaseCharacters);
+//   console.log(getNumericCharacters);
+//   console.log(getUppercaseCharacters);
+//   console.log(getSpecialCharacters);
 
-  if (getSpecialCharacters === false && getNumericCharacters === false &&
-     getLowercaseCharacters === false && getUppercaseCharacters=== false) {
-      alert('Must choose at least one kind of type of characters');
-  } else {
-    alert('Success! Thank you!')
-  }
-}
-getPasswordOptions()
+//   if (getSpecialCharacters === false && getNumericCharacters === false &&
+//      getLowercaseCharacters === false && getUppercaseCharacters=== false) {
+//       alert('Must choose at least one kind of type of characters');
+//   } else {
+//     alert('Success! Thank you!')
+//   }
+// }
+// getPasswordOptions()
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-    return arr [Math.floor(Math.random() * arr.lengh)]
+  // return arr
+
+  for (var i = 0; i < arr.length; i++) {
+    return arr [Math.floor(Math.random() * arr.length)];
+  }
 }
 
 let randomSpecialCharacters = getRandom(specialCharacters);
@@ -138,22 +142,22 @@ console.log(randomNumericCharacters);
 console.log(randomLowercaseCharacters);
 console.log(randomUppercaseCharacters);
 
-                                              
-// Function to generate password with user input
-function generatePassword() {
 
-}
+// // Function to generate password with user input
+// function generatePassword() {
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+// }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+// // Get references to the #generate element
+// var generateBtn = document.querySelector('#generate');
 
-  passwordText.value = password;
-}
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
